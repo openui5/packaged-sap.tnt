3f8989510d394d933d40aa5b6e9387bc5f3c8d62
OpenUI5 <openui5@sap.com>
2016-12-08 17:37:38 +0100

    1.40.13

--- resources/sap/tnt/NavigationList.js
+++ resources/sap/tnt/NavigationList.js
@@ -21,7 +21,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/m/P
 		 * @extends sap.ui.core.Control
 		 *
 		 * @author SAP SE
-		 * @version 1.40.12
+		 * @version 1.40.13
 		 *
 		 * @constructor
 		 * @public
