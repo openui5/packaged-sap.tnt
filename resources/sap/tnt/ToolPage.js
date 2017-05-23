590603885a5538520c817bcd642ae6a83e7f0a07
OpenUI5 <openui5@sap.com>
2017-05-23 15:27:34 +0200

    1.38.22

--- resources/sap/tnt/ToolPage.js
+++ resources/sap/tnt/ToolPage.js
@@ -20,7 +20,7 @@ sap.ui.define(['./library', 'sap/ui/core/Control', 'sap/ui/Device', 'sap/ui/core
 		 * @extends sap.ui.core.Control
 		 *
 		 * @author SAP SE
-		 * @version 1.38.21
+		 * @version 1.38.22
 		 *
 		 * @constructor
 		 * @public
