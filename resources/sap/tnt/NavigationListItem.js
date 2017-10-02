10fd2e1d98b7cf136751cf693322a1ab33174e7e
OpenUI5 <openui5@sap.com>
2017-10-02 17:01:21 +0200

    1.44.21

--- resources/sap/tnt/NavigationListItem.js
+++ resources/sap/tnt/NavigationListItem.js
@@ -17,7 +17,7 @@ sap.ui.define(["jquery.sap.global", "./library", "sap/ui/core/Item",
 		 * @extends sap.ui.core.Item
 		 *
 		 * @author SAP SE
-		 * @version 1.44.20
+		 * @version 1.44.21
 		 *
 		 * @constructor
 		 * @public
