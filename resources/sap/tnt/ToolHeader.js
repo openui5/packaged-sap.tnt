71e217dbd0eee58abd7f40e1e2b63aad8a9a5a5e
OpenUI5 <openui5@sap.com>
2017-10-11 12:47:12 +0200

    1.38.30

--- resources/sap/tnt/ToolHeader.js
+++ resources/sap/tnt/ToolHeader.js
@@ -28,7 +28,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control',
 		 * @extends sap.m.OverflowToolbar
 		 *
 		 * @author SAP SE
-		 * @version 1.38.29
+		 * @version 1.38.30
 		 *
 		 * @constructor
 		 * @public
