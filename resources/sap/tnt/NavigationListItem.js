b7fddc3c37a4a1407fa4fc8f8ab08022abc15a36
OpenUI5 <openui5@sap.com>
2018-03-12 18:58:02 +0100

    1.44.29

--- resources/sap/tnt/NavigationListItem.js
+++ resources/sap/tnt/NavigationListItem.js
@@ -17,7 +17,7 @@ sap.ui.define(["jquery.sap.global", "./library", "sap/ui/core/Item",
 		 * @extends sap.ui.core.Item
 		 *
 		 * @author SAP SE
-		 * @version 1.44.28
+		 * @version 1.44.29
 		 *
 		 * @constructor
 		 * @public
--- resources/sap/tnt/ToolPage.js
+++ resources/sap/tnt/ToolPage.js
@@ -20,7 +20,7 @@ sap.ui.define(['./library', 'sap/ui/core/Control', 'sap/ui/Device', 'sap/ui/core
 		 * @extends sap.ui.core.Control
 		 *
 		 * @author SAP SE
-		 * @version 1.44.28
+		 * @version 1.44.29
 		 *
 		 * @constructor
 		 * @public
