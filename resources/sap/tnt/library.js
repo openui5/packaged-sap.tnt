e71c383e8544475db81512d791827cb2c7fbb531
OpenUI5 <openui5@sap.com>
2018-05-02 21:34:55 +0200

    1.38.35

--- resources/sap/tnt/library.js
+++ resources/sap/tnt/library.js
@@ -19,14 +19,14 @@ sap.ui.define(['jquery.sap.global',
 	 * @namespace
 	 * @name sap.tnt
 	 * @author SAP SE
-	 * @version 1.38.34
+	 * @version 1.38.35
 	 * @public
 	 */
 
 	// delegate further initialization of this library to the Core
 	sap.ui.getCore().initLibrary({
 		name : 'sap.tnt',
-		version: '1.38.34',
+		version: '1.38.35',
 		dependencies : ['sap.ui.core','sap.m'],
 		types: [],
 		interfaces: [],
