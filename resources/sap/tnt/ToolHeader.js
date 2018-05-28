ef0f670d862166928a6c2ffdd2a49172e643937e
OpenUI5 <openui5@sap.com>
2018-05-28 14:44:54 +0200

    1.56.1

--- resources/sap/tnt/ToolHeader.js
+++ resources/sap/tnt/ToolHeader.js
@@ -44,7 +44,7 @@ sap.ui.define([
 		 * @extends sap.m.OverflowToolbar
 		 *
 		 * @author SAP SE
-		 * @version 1.56.0
+		 * @version 1.56.1
 		 *
 		 * @constructor
 		 * @public
--- resources/sap/tnt/library.js
+++ resources/sap/tnt/library.js
@@ -22,14 +22,14 @@ sap.ui.define([
 	 * @namespace
 	 * @name sap.tnt
 	 * @author SAP SE
-	 * @version 1.56.0
+	 * @version 1.56.1
 	 * @public
 	 */
 
 	// delegate further initialization of this library to the Core
 	sap.ui.getCore().initLibrary({
 		name : "sap.tnt",
-		version: "1.56.0",
+		version: "1.56.1",
 		dependencies : ["sap.ui.core", "sap.m"],
 		types: [
 			"sap.tnt.RenderMode",
