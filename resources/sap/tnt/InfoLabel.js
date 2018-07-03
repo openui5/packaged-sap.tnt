1f4e560ab0a39c602f2caa96d5ae9621f6979efc
OpenUI5 <openui5@sap.com>
2018-07-03 11:29:12 +0200

    1.56.3

--- resources/sap/tnt/InfoLabel.js
+++ resources/sap/tnt/InfoLabel.js
@@ -42,7 +42,7 @@ sap.ui.define(["./library", "sap/ui/core/Control", "sap/ui/core/library", "./Inf
 		 * @extends sap.ui.core.Control
 		 *
 		 * @author SAP SE
-		 * @version 1.56.2
+		 * @version 1.56.3
 		 *
 		 * @constructor
 		 * @public
--- resources/sap/tnt/ToolHeader.js
+++ resources/sap/tnt/ToolHeader.js
@@ -44,7 +44,7 @@ sap.ui.define([
 		 * @extends sap.m.OverflowToolbar
 		 *
 		 * @author SAP SE
-		 * @version 1.56.2
+		 * @version 1.56.3
 		 *
 		 * @constructor
 		 * @public
