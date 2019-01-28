e4c19d9f15754344c562e6afb10a8b2d7735625c
OpenUI5 <openui5@sap.com>
2019-01-28 15:23:52 +0100

    1.56.18

--- resources/sap/tnt/NavigationListItem.js
+++ resources/sap/tnt/NavigationListItem.js
@@ -17,7 +17,7 @@ sap.ui.define(["jquery.sap.global", "./library", "sap/ui/core/Item",
 		 * @extends sap.ui.core.Item
 		 *
 		 * @author SAP SE
-		 * @version 1.56.16
+		 * @version 1.56.18
 		 *
 		 * @constructor
 		 * @public
@@ -739,6 +739,6 @@ sap.ui.define(["jquery.sap.global", "./library", "sap/ui/core/Item",
 
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2018 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2019 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
